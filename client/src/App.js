806607f7283866b36ed3d3e1f83056a2ff778516
Jared Grove <jaredkgrove@gmail.com>
2019-10-16 09:16:59 -0500

    deleted logo.svg and tested fetch in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,13 @@ import React from 'react';
 import './App.css';
 import { NavLink } from 'react-router-dom';
 
-function App() {
+class App extends React.Component {
+  componentDidMount(){
+    fetch('http://localhost:3000/api/v1/sketches/1')
+    .then(resp => resp.json())
+    .then(json => console.log(json.included[0].attributes.properties))
+  }
+  render(){
   return (
     <div className="App">
       <header className="App-header">
@@ -24,7 +30,7 @@ function App() {
           {/* {routerProps => <Sketch {...routerProps} sketch={this.state.sketch}/>} */}
       </header>
     </div>
-  );
+  )}
 }
 
 export default App;
