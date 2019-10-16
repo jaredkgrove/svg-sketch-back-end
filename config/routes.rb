018966095d243be600328a15dda2a0104d970729
Jared Grove <jaredkgrove@gmail.com>
2019-10-16 09:17:55 -0500

    nested elements in sketch

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users
-      resources :sketches
-      resources :elements
+      resources :sketches do
+        resources :elements
+      end
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
