c0d6c5c9c67dd9c0157be75562fe7522a0b04f57
Jared Grove <jaredkgrove@gmail.com>
2019-10-24 13:06:34 -0500

    added UPDATE_LINE_WIDTH action

--- client/src/Reducers/sketchSettingsReducer.js
+++ client/src/Reducers/sketchSettingsReducer.js
@@ -3,8 +3,11 @@ const sketchSettingsReducer = (state = {lineColor:{h: 0, s: 100, l:50}, lineWidt
     switch(action.type) {
       case 'UPDATE_LINE_COLOR':
         return {...state, lineColor: {h: action.payload.h, s: action.payload.s, l: action.payload.l}}
-        case 'UPDATE_FILL_COLOR':
-            return {...state, fillColor: {h: action.payload.h, s: action.payload.s, l: action.payload.l}}
+      case 'UPDATE_FILL_COLOR':
+          return {...state, fillColor: {h: action.payload.h, s: action.payload.s, l: action.payload.l}}
+      case 'UPDATE_LINE_WIDTH':
+          return {...state, lineWidth: action.payload}
+
     
       default:
         return state;
