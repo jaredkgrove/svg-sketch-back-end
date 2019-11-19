36ed95229417671cf4a61a7711a1209323548567
Jared Grove <jaredkgrove@gmail.com>
2019-11-19 12:55:05 -0600

    sized background image to cover button and changed aspect ratio of button

--- client/src/components/lineTypeSelector.jsx
+++ client/src/components/lineTypeSelector.jsx
@@ -2,16 +2,20 @@ import React from 'react';
 
  const LineTypeSelector = (props) => {
 
-    const handleClick = e => (props.handleUpdate(e.target.value))
+    const handleClick = (e) => {
+        console.log(e.target.value)
+        props.handleUpdate(e.target.value)
+    }
 
     const isCurrentSetting = value => (value === props.lineType)
 
     return (
         <>
-            <button className={isCurrentSetting('Circle') ? 'selected' : null} onClick={handleClick} value='Circle' style={{height:'50%', margin:'1%'}}> Circle </button>
-            <button className={isCurrentSetting('Line') ? 'selected' : null} onClick={handleClick} value='Line' style={{height:'50%', margin:'1%'}}> Line </button>
-            <button className={isCurrentSetting('Rectangle') ? 'selected' : null} onClick={handleClick} value='Rectangle' style={{height:'50%', margin:'1%'}}> Rect </button>
-            <button className={isCurrentSetting('Polyline') ? 'selected' : null} onClick={handleClick} value='Polyline' style={{height:'50%', margin:'1%'}}> Free </button>
+            
+            <button className={isCurrentSetting('Circle') ? 'selected' : null} onClick={handleClick} value='Circle' style={{paddingTop:'21%', margin:'1%', background: "url(/images/Circle.png)", backgroundSize: 'cover'}}> </button>
+            <button className={isCurrentSetting('Line') ? 'selected' : null} onClick={handleClick} value='Line' style={{paddingTop:'21%', margin:'1%', background: "url(/images/Line.png)",  backgroundSize: 'cover'}}>  </button>
+            <button className={isCurrentSetting('Rectangle') ? 'selected' : null} onClick={handleClick} value='Rectangle' style={{paddingTop:'21%', margin:'1%', background: "url(/images/Rectangle.png)", backgroundSize: 'cover'}}> </button>
+            <button className={isCurrentSetting('Polyline') ? 'selected' : null} onClick={handleClick} value='Polyline' style={{paddingTop:'21%', margin:'1%', background: "url(/images/Free.png)", backgroundSize: 'cover'}}>  </button>
         </>
     )
  }
