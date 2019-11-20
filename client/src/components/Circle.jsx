a60043ea8bbe40b9f5b897dc30ab69f32e240f9c
Jared Grove <jaredkgrove@gmail.com>
2019-11-19 18:54:28 -0600

    removed client

--- client/src/components/Circle.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
- const Circle = ({ properties }) => (
-    <circle cx={properties.cx} cy={properties.cy} r={properties.r} stroke={properties.stroke} fill={properties.fill} strokeWidth={properties.stroke_width}/>
- );
-
- export default Circle
--- client/src/components/ColorIndicator.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
- const ColorIndicator = ({color}) => {
-    return (
-        <div className='color-indicator' style={{backgroundColor:`hsl(${color.h},${color.s}%,${color.l}%)`}}></div>
-    )
- }
-
- export default ColorIndicator
