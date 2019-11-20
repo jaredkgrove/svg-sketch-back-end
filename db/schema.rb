c3226d10d52263df412e1af94d6b5c18cfe0c67c
Jared Grove <jaredkgrove@gmail.com>
2019-11-19 18:29:25 -0600

    deploy

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_10_24_200605) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "circles", force: :cascade do |t|
     t.decimal "cx"
     t.decimal "cy"
